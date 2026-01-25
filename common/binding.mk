30f724e84f8138d04c0f16f5318ecd96b617d5e7
Ingy döt Net <ingy@ingy.net>
2026-01-25 07:44:46 -0800

    Refactor binding Makefiles for Windows

--- common/binding.mk
+++ common/binding.mk
@@ -1,5 +1,9 @@
 BINDING-NAME := $(shell basename $(shell pwd))
 
+ifeq ($(OS-NAME),windows)
+  override export PATH := $(LIBYS)/lib:$(PATH)
+endif
+
 release:: release-deps
 	$(ROOT)/util/release release-$(BINDING-NAME)
 
