a1ea5593c9e35e35b8bfc00e262ac4ae5f8cf7fc
Ingy döt Net <ingy@ingy.net>
2026-03-01 20:07:19 -0800

    Makefile update for more versatile build/test

--- common/init.mk
+++ common/init.mk
@@ -1,4 +1,10 @@
-M := ../.cache/makes
+# XXX Avoiding ingy's KVM bug here
+ifeq (vm1,$(and $(filter ingy,$(USER)),$(shell hostname)))
+INGY-LOCAL-DIR := /tmp/yamlstar-local
+export MAKES_LOCAL_DIR := $(INGY-LOCAL-DIR)
+endif
+
+M ?= ../.cache/makes
 $(shell [ -d $M ] || git clone -q https://github.com/makeplus/makes $M)
 include $M/init.mk
 include $M/git.mk
